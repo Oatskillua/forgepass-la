c5d8cf334ae437735e4e5a254ebd9241d3eb62c2
Oatskillua <michaelaquarker@gmail.com>
2026-05-23 22:14:14 -0700

    Render home sections from configuration

---
+++ src/data/homeSections.js
@@ -0,0 +1,13 @@
+import FeatureSection from '../components/home/FeatureSection'
+import SystemPreviewSection from '../components/home/SystemPreviewSection'
+import LiveMetricsSection from '../components/home/LiveMetricsSection'
+import HomeRoadmapSection from '../components/home/HomeRoadmapSection'
+import CityIntelligenceSection from '../components/home/CityIntelligenceSection'
+
+export const homeSections = [
+  FeatureSection,
+  SystemPreviewSection,
+  LiveMetricsSection,
+  HomeRoadmapSection,
+  CityIntelligenceSection,
+]
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,11 @@ import Header from '../components/Header'
 import Footer from '../components/Footer'
 import WaitlistForm from '../components/WaitlistForm'
 import MobileNav from '../components/MobileNav'
-import HomeRoadmapSection from '../components/home/HomeRoadmapSection'
 
 import HeroSection from '../components/home/HeroSection'
-import FeatureSection from '../components/home/FeatureSection'
-import SystemPreviewSection from '../components/home/SystemPreviewSection'
-import LiveMetricsSection from '../components/home/LiveMetricsSection'
-import CityIntelligenceSection from '../components/home/CityIntelligenceSection'
 import TrustStrip from '../components/home/TrustStrip'
+
+import { homeSections } from '../data/homeSections'
 
 export default function Home() {
   return (
@@ -22,15 +19,9 @@ export default function Home() {
         <section className="mx-auto max-w-7xl px-5 py-10 pb-28 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
           <HeroSection />
 
-          <FeatureSection />
-
-          <SystemPreviewSection />
-
-          <LiveMetricsSection />
-
-          <HomeRoadmapSection />
-
-          <CityIntelligenceSection />
+          {homeSections.map((Section) => (
+            <Section key={Section.name} />
+          ))}
 
           <section
             id="waitlist"
